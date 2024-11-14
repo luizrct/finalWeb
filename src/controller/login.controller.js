d9a01e35be781f2c09ce55554cf7d0c553d4c347
Luiz <luizchavestillio1@gmail.com>
2024-11-14 16:25:24 -0300

    quinto

--- src/controller/login.controller.js
+++ src/controller/login.controller.js
@@ -10,12 +10,20 @@ class LoginController{
         res.render("register", {title:"Registro", showError:false})
     }
 
-    postRegister(req, res){
+    async postRegister(req, res){
         const result = validationResult(req)
         if(result.isEmpty()){
-            res.redirect("/login")
+            if(verificador1(req.body.email)){
+                const name = req.body.nome
+                const email = req.body.email
+                const password = req.body.password
+                await User.create({name, email, password})
+                res.redirect("/login")
+            }else{
+                 res.render("register", {title:"Registro", showError:true, tipoErro:2})
+            }
         }else{
-            res.render("register", {title:"Registro", showError:true})
+            res.render("register", {title:"Registro", showError:true, tipoErro:1})
         }
     }
 
@@ -27,8 +35,20 @@ class LoginController{
     
 }
 
-//função que percorre banco de dados para verificar se já existe um usuário com o mesmo email
-function verificao1(){
 
+//função que percorre banco de dados para verificar se já existe um usuário com o mesmo email
+async function verificador1(email){
+    try{
+        const usuario = User.findOne({ where: {email}})
+        if(usuario){
+            return true
+        }else{
+            return false
+        }
+    }catch (error){
+        console.log(error)
+        return false
+    }
 }
+
 export default LoginController
--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import {express, port} from "./config/config.js"
-
+import db from "./database/db.js"
 
 express.listen(port, async() => {
     console.log(`Example app listening on port ${port}`);
+    await db.sync({ force: false });
+    console.log('All models were synchronized successfully.');
 });
